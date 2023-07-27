f86cdee5bb6e61f57d17def8f90cf0106071e36f
Behruzaxi <ebehruz846@gmail.com>
2023-07-27 12:02:32 +0500

    s

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Section from './components/Section/Section'
+
+export default function App() {
+  return (
+<>
+<Section/>
+</>
+  )
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,400 @@
+import React from "react";
+
+export default function Section() {
+  return (
+    <>
+      <section className="w-full divide-y rounded divide-slate-200 mt-30 w-1100px ml-200px bg-gray-100">
+        <details class="p-4 group" open>
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            1. Vaqt o'tgandan keyin kursga yangiliklar qo'shiladimi? Agar qo'shilsa, ulardan biz foydalana olamizmi 
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac01 desc-ac01"
+            >
+              <title id="title-ac01">Open icon</title>
+              <desc id="desc-ac01">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            Tailwind CSS works by scanning all of your HTML files, JavaScript
+            components, and any other templates for class names, generating the
+            corresponding styles and then writing them to a static CSS file.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            2. Kursni tamomlaganlar ish bilan ta'minlanadimi?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac02 desc-ac02"
+            >
+              <title id="title-ac02">Open icon</title>
+              <desc id="desc-ac02">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            The simplest and fastest way to get up and running with Tailwind CSS
+            from scratch is with the Tailwind CLI tool. The CLI is also
+            available as a standalone executable if you want to use it without
+            installing Node.js. Install tailwindcss via npm, and create your
+            tailwind.config.js file.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            3. Kursni tamomlagandan keyin sertifikat beriladimi?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac03 desc-ac03"
+            >
+              <title id="title-ac03">Open icon</title>
+              <desc id="desc-ac03">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            Expertly made, responsive, accessible components in React and HTML
+            ready to be used on your website or app. Just copy and paste them on
+            your Tailwind CSS project.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            4. Kurs uchun to'lovni qanday oshirish mumkin?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac04 desc-ac04"
+            >
+              <title id="title-ac04">Open icon</title>
+              <desc id="desc-ac04">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            All components can be copied and pasted and easily implemented in
+            your tailwind css projects. You can choose which language you want
+            to copy the desired component and just hover and click on the
+            component you need and paste it on your project.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            5. Kursdan foyda olmasam pulim qaytarib beriladimi?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac04 desc-ac04"
+            >
+              <title id="title-ac04">Open icon</title>
+              <desc id="desc-ac04">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            All components can be copied and pasted and easily implemented in
+            your tailwind css projects. You can choose which language you want
+            to copy the desired component and just hover and click on the
+            component you need and paste it on your project.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            6. Kursni sotib olsam, undan doim foydalana olamanmi?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac04 desc-ac04"
+            >
+              <title id="title-ac04">Open icon</title>
+              <desc id="desc-ac04">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            All components can be copied and pasted and easily implemented in
+            your tailwind css projects. You can choose which language you want
+            to copy the desired component and just hover and click on the
+            component you need and paste it on your project.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            7. Platformadagi videodarslarni yuklab olsam bo'ladimi?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac04 desc-ac04"
+            >
+              <title id="title-ac04">Open icon</title>
+              <desc id="desc-ac04">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            All components can be copied and pasted and easily implemented in
+            your tailwind css projects. You can choose which language you want
+            to copy the desired component and just hover and click on the
+            component you need and paste it on your project.
+          </p>
+        </details>
+        <details class="p-4 group">
+          <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+            8. Bitta profildan necha kishi foydalansa bo'ladi
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="1.5"
+              aria-labelledby="title-ac04 desc-ac04"
+            >
+              <title id="title-ac04">Open icon</title>
+              <desc id="desc-ac04">
+                icon that represents the state of the summary
+              </desc>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </summary>
+          <p class="mt-4 text-slate-500">
+            All components can be copied and pasted and easily implemented in
+            your tailwind css projects. You can choose which language you want
+            to copy the desired component and just hover and click on the
+            component you need and paste it on your project.
+          </p>
+        </details>
+            <details class="p-4 group">
+            <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+                9. Darslarni telefonda ko'rsam bo'ladimi?
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+                aria-labelledby="title-ac04 desc-ac04"
+                >
+                <title id="title-ac04">Open icon</title>
+                <desc id="desc-ac04">
+                    icon that represents the state of the summary
+                </desc>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4v16m8-8H4"
+                />
+                </svg>
+            </summary>
+            <p class="mt-4 text-slate-500">
+                All components can be copied and pasted and easily implemented in
+                your tailwind css projects. You can choose which language you want
+                to copy the desired component and just hover and click on the
+                component you need and paste it on your project.
+            </p>
+            </details>
+            <details class="p-4 group">
+            <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+                10. Saytdan qanday qlib ro'yxatdan o'tiladi ?
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+                aria-labelledby="title-ac04 desc-ac04"
+                >
+                <title id="title-ac04">Open icon</title>
+                <desc id="desc-ac04">
+                    icon that represents the state of the summary
+                </desc>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4v16m8-8H4"
+                />
+                </svg>
+            </summary>
+            <p class="mt-4 text-slate-500">
+                All components can be copied and pasted and easily implemented in
+                your tailwind css projects. You can choose which language you want
+                to copy the desired component and just hover and click on the
+                component you need and paste it on your project.
+            </p>
+            </details>
+            <details class="p-4 group">
+            <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+                11. Sertifikatni yuklab olsam bo'ladimi?
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+                aria-labelledby="title-ac04 desc-ac04"
+                >
+                <title id="title-ac04">Open icon</title>
+                <desc id="desc-ac04">
+                    icon that represents the state of the summary
+                </desc>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4v16m8-8H4"
+                />
+                </svg>
+            </summary>
+            <p class="mt-4 text-slate-500">
+                All components can be copied and pasted and easily implemented in
+                your tailwind css projects. You can choose which language you want
+                to copy the desired component and just hover and click on the
+                component you need and paste it on your project.
+            </p>
+            </details>
+            <details class="p-4 group">
+            <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+                12. Sertifikatni olganimdan so'ng ismini yoki  sertifikat formatini o'zgartirib, qayta yuklasa bo'ladimi?
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+                aria-labelledby="title-ac04 desc-ac04"
+                >
+                <title id="title-ac04">Open icon</title>
+                <desc id="desc-ac04">
+                    icon that represents the state of the summary
+                </desc>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4v16m8-8H4"
+                />
+                </svg>
+            </summary>
+            <p class="mt-4 text-slate-500">
+                All components can be copied and pasted and easily implemented in
+                your tailwind css projects. You can choose which language you want
+                to copy the desired component and just hover and click on the
+                component you need and paste it on your project.
+            </p>
+            </details>
+            <details class="p-4 group">
+            <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
+                13. Shaxsiy kabinetimga kira olmayapman.Nima qilsam bo'ladi?
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+                aria-labelledby="title-ac04 desc-ac04"
+                >
+                <title id="title-ac04">Open icon</title>
+                <desc id="desc-ac04">
+                    icon that represents the state of the summary
+                </desc>
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4v16m8-8H4"
+                />
+                </svg>
+            </summary>
+            <p class="mt-4 text-slate-500">
+                All components can be copied and pasted and easily implemented in
+                your tailwind css projects. You can choose which language you want
+                to copy the desired component and just hover and click on the
+                component you need and paste it on your project.
+            </p>
+            </details>
+        
+      </section>
+    </>
+  );
+}
